abc045706753bac795d2a6ddf60c1e9c1150d9cf
Alex Sverdlov <sverdlov.alexandr@gmail.com>
2021-03-14 22:54:44 +0200

    finish

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import Loader from 'react-loader-spinner';
 import Searchbar from './components/Searchbar';
 import Button from './components/Button';
 import ImageGallery from './components/ImageGallery';
@@ -35,7 +36,13 @@ class App extends React.Component {
           page: prevState.page + 1,
         }));
       })
-      .finally(this.setState({ isloading: false }));
+      .finally(() => {
+        window.scrollTo({
+          top: document.documentElement.scrollHeight,
+          behavior: 'smooth',
+        });
+        this.setState({ isloading: false });
+      });
   };
 
   handleSubmit = searchtext => {
@@ -58,7 +65,15 @@ class App extends React.Component {
           onOpenModal={this.toggleModal}
         />
 
-        {this.state.isloading && <h1>Загружаем</h1>}
+        {this.state.isloading && (
+          <Loader
+            type="Puff"
+            color="#00BFFF"
+            height={100}
+            width={100}
+            timeout={3000} //3 secs
+          />
+        )}
         {this.state.datahits.length > 0 && !this.state.isloading && (
           <Button onClick={this.getImageFromAPI} />
         )}
--- src/components/ImageGalleryItem.js
+++ src/components/ImageGalleryItem.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import styles from './ImageGallery.module.css';
 
-const ImageGalleryItem = ({ url, largeUrl }) => {
-  return (
-    <li className={styles.ImageGalleryItem}>
-      <img
-        src={url}
-        alt=""
-        className={styles.ImageGalleryItemimage}
-        data-largeurl={largeUrl}
-      />
-    </li>
-  );
-};
+const ImageGalleryItem = ({ url, largeUrl }) => (
+  <li className={styles.ImageGalleryItem}>
+    <img
+      src={url}
+      alt=""
+      className={styles.ImageGalleryItemimage}
+      data-largeurl={largeUrl}
+    />
+  </li>
+);
 
 export default ImageGalleryItem;
